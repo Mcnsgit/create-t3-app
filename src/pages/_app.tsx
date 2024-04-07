c6a90dd719e39a3b3e8ee1d05e5603e0dad0ff40
Mcnsgit <130765870+Mcnsgit@users.noreply.github.com>
2024-04-07 15:44:23 +0100

    schema n index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import { type AppType } from "next/app";
+import  type{ AppType , AppProps} from "next/app";
 import { Inter } from "next/font/google";
 
 import { api } from "import/utils/api";
+import { ClerkProvider } from '@clerk/nextjs'
 
+import {neobrutalism} from "@clerk/themes";
 import "import/styles/globals.css";
 
 const inter = Inter({
@@ -11,9 +13,11 @@ const inter = Inter({
 });
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
+    <ClerkProvider appearance={{baseTheme: neobrutalism}}  {...pageProps} >
     <main className={`font-sans ${inter.variable}`}>
       <Component {...pageProps} />
     </main>
+    </ClerkProvider>
   );
 };
 
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,32 +1,36 @@
 import { z } from "zod";
-
+import { PrismaClient } from "import/server/db";
 import { createTRPCRouter, publicProcedure } from "import/server/api/trpc";
 
+const prisma = new PrismaClient();
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
-
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
+  .input(z.object({ text: z.string() }))
+  .query(({ input }) => {
+    return {
+      greeting: `Hello ${input.text}`,
+    };
   }),
-});
+  getAll: publicProcedure.query(({ ctx }) => {
+    return  ctx.prisma.post.findMany()
+    })
+  
+  // create: publicProcedure
+    // .input(z.object({ name: z.string().min(1) }))
+    // .mutation(async ({ ctx, input }) => {
+    //   // simulate a slow db call
+    //   await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    //   return ctx.db.post.create({
+    //     data: {
+    //       name: input.name,
+    //     },
+    //   });
+    // }),
+
+//   getLatest: publicProcedure.query(({ ctx }) => {
+//     return ctx.db.post.findFirst({
+//       orderBy: { createdAt: "desc" },
+//     });
+//   }),
+// });
